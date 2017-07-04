6528d4867b74af98b842935fed27ac6701eff334
Eric Cao <spacewoox@gmail.com>
2017-07-04 16:45:43 +0200

    create a fakeIdentify to mock api

--- src/earlybirds.test.js
+++ src/earlybirds.test.js
@@ -6,12 +6,16 @@ var mock = new MockAdapter(axios);
 
 describe('test test', () => {
 
-  test('Identify should return a promise', () => {
+  function fakeIdentify() {
     let fakeTrackerKey = 'fakeTrackerKey';
     mock.onPost(`http://api.early-birds.fr/tracker/fakeTrackerKey/identify`).reply(200, {});
     const eb = new Eb();
     eb.init(fakeTrackerKey);
-    const identifyReturnValue = eb.identify({});
+    return eb.identify(...arguments);
+  }
+
+  test('Identify should return a promise', () => {
+    const identifyReturnValue = fakeIdentify({});
     expect(identifyReturnValue.then).toBeDefined();
   });
 });
