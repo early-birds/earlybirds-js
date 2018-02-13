cd2bf99d6b493ca6940907924e42741ce9a835e0
Auvray Nicolas <nicolas.auvray@early-birds.io>
2018-02-13 17:20:34 +0100

    work in progress 1

--- src/eb.js
+++ src/eb.js
@@ -1,136 +1,168 @@
-import makeSingleton from './utils/singleton'
-import Cookies from './utils/Cookies'
-import Config from '../config'
-import { Encode } from './utils/Utils'
+import makeSingleton from './utils/singleton';
+import Cookies from './utils/Cookies';
+import Config from '../config';
+import {
+  Encode
+} from './utils/Utils';
 import {
   cookieDomainMatchGivenHost,
-  shouldInitiateIdentifyRequest
-} from './checks/profileValidationCheck'
+  shouldInitiateIdentifyRequest,
+} from './checks/profileValidationCheck';
 
-import { checkActivitiesInputs } from './checks/trackActivityCheck'
+import {
+  checkActivitiesInputs
+} from './checks/trackActivityCheck';
 
 class Eb {
   constructor(trackerKey) {
     this.defaultProfile = {
       hash: null,
-      lastIdentify: null
-    }
+      lastIdentify: null,
+    };
     this.defaultCookieConfig = {
-      expires: 365
-    }
-    this.hostname = document.location.hostname
-    this.profile = this.retrieveEbProfile()
-    this.trackerKey = trackerKey || null
+      expires: 365,
+    };
+    this.hostname = document.location.hostname;
+    this.profile = this.retrieveEbProfile();
+    this.trackerKey = trackerKey || null;
   }
 
   retrieveEbProfile() {
-    return Cookies.getCookie('eb-profile') || null
+    return Cookies.getCookie('eb-profile') || null;
   }
 
   init(trackerKey) {
-    this.trackerKey = trackerKey
+    this.trackerKey = trackerKey;
   }
 
-  identify(newProfile = { profile: {} }, options = {
-    cookieDuration: 90
+  identify(newProfile = {
+    profile: {},
+  }, options = {
+    cookieDuration: 90,
   }) {
     if (!this.trackerKey) return null;
     if (shouldInitiateIdentifyRequest(newProfile, this.profile)) {
       return this.identifyRequest(newProfile)
-        .then(response => {
+        .then((response) => {
           const profile = {
             ...response.profile,
             lastIdentify: new Date().getTime(),
-            hash: Encode(newProfile)
-          }
+            hash: Encode(newProfile),
+          };
           const cookieConfig =
             cookieDomainMatchGivenHost(response.cookie, this.hostname) ?
-              response.cookie : this.defaultCookieConfig
+              response.cookie : this.defaultCookieConfig;
 
           Cookies.setCookie(
             'eb-profile',
-            JSON.stringify(profile),
-            cookieConfig
+            `${profile.id}:${profile.hash}:${profile.lastUpdate}`,
+            cookieConfig,
           );
 
-          this.profile = profile
+          this.profile = profile;
           return response;
         })
-        .catch(err => {
+        .catch((err) => {
           throw err;
-        })
+        });
     }
     return new Promise(r => r(this.profile));
   }
 
   identifyRequest(profile) {
-    const url = `\
-${Config.HTTP_PROTOCOL}\
-${Config.API_URL}\
-/tracker/${this.trackerKey}/identify`;
+    const url = `${Config.HTTP_PROTOCOL}${Config.API_URL}/tracker/${this.trackerKey}/identify`;
     return fetch(url, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(profile)
-    })
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(profile),
+      })
       .then(x => x.json());
   }
 
-  getRecommendations(widgetId, { rescorerParams = {}, variables = {} } = {}) {
+  getRecommendations(widgetId, {
+    rescorerParams = {},
+    variables = {},
+  } = {}) {
     if (!widgetId) return Promise.reject(new Error('WidgetId is mandatory'));
     if (!this.profile) {
       return new Promise((r, j) => j('Earlybirds error: Not identified'));
     }
-    const url = `\
-${Config.HTTP_PROTOCOL}\
-${Config.API_URL}\
-/widget/${widgetId}\
-/recommendations/${this.profile.id}`
+    const url = `${Config.HTTP_PROTOCOL}${Config.API_URL}/widget/${widgetId}/recommendations/${this.profile.id}`;
     return fetch(`${url}?rescorerParams=${JSON.stringify(rescorerParams)}&variables=${JSON.stringify(variables)}`)
       .then(x => x.json())
-      .catch(err => {
+      .catch((err) => {
         throw err;
       });
   }
 
-  getRecosForCluster(widgetId, clusterId, { rescorerParams = {}, variables = {} } = {}) {
+  getRecosForCluster(widgetId, clusterId, {
+    rescorerParams = {},
+    variables = {},
+  } = {}) {
     if (!widgetId) return Promise.reject(new Error('WidgetId is mandatory'));
     if (!clusterId) return Promise.reject(new Error('Earlybirds error: ClusterId not provided'));
     if (!this.profile) {
       return new Promise((r, j) => j('Earlybirds error: Not identified'));
     }
-    const url = `\
-${Config.HTTP_PROTOCOL}\
-${Config.API_URL}\
-/widget/${widgetId}\
-/recommendations/cluster/${clusterId}`
+    const url = `${Config.HTTP_PROTOCOL}${Config.API_URL}/widget/${widgetId}/recommendations/cluster/${clusterId}`;
+
     return fetch(`${url}?rescorerParams=${JSON.stringify(rescorerParams)}&variables=${JSON.stringify(variables)}`)
       .then(x => x.json())
-      .catch(err => {
+      .catch((err) => {
         throw err;
       });
   }
 
-  getRecommendationsMulti(widgetIds, { rescorerParams = {}, variables = {} } = {}) {
+  getRecommendationsMulti(widgetIds, {
+    rescorerParams = {},
+    variables = {},
+  } = {}) {
     if (!widgetIds) return Promise.reject(new Error('WidgetIds are mandatory'));
     if (!this.profile) return Promise.reject(new Error('Earlybirds error: Not identified'));
-    const url = `\
-${Config.HTTP_PROTOCOL}\
-${Config.API_URL}\
-/widget/multi/${widgetIds}\
-/recommendations/${this.profile.id}`
+    const url = `${Config.HTTP_PROTOCOL}${Config.API_URL}/widget/multi/${widgetIds}/recommendations/${this.profile.id}`;
+
     return fetch(`${url}?rescorerParams=${JSON.stringify(rescorerParams)}&variables=${JSON.stringify(variables)}`)
       .then(x => x.json())
-      .catch(err => {
+      .catch((err) => {
         throw err;
       });
+  }
+
+  static getNavigatorSegments() {
+    const segments = {
+      navigator_appCodeName: window.navigator.appCodeName,
+      navigator_appName: window.navigator.appName,
+      navigator_appVersion: window.navigator.appVersion,
+      navigator_cookieEnabled: window.navigator.cookieEnabled,
+      navigator_doNotTrack: window.navigator.doNotTrack,
+      navigator_language: window.navigator.language,
+      navigator_platform: window.navigator.platform,
+      navigator_product: window.navigator.product,
+      navigator_productSub: window.navigator.productSub,
+      navigator_userAgent: window.navigator.userAgent,
+      navigator_vendor: window.navigator.vendor,
+      navigator_vendorSub: window.navigator.vendorSub,
+    };
+
+    try {
+      window.localStorage.setItem('testeb', 'testeb');
+      window.localStorage.removeItem('testeb');
+      segments.navigator_localStorageEnabled = true;
+    } catch (e) {
+      segments.navigator_localStorageEnabled = false;
+    }
+
+    if (window.screen.width && window.screen.height) {
+      segments.screen_resolution = `${window.screen.width}x${window.screen.height}`;
+    }
+
+    return segments;
   }
 
   trackActivity(activities) {
-
     if (!activities) {
       return Promise.reject(new Error('no activities provided'))
     }
@@ -139,22 +171,28 @@ ${Config.API_URL}\
       activities = [activities];
     }
 
-    const checkActivityInputsErr = checkActivitiesInputs(activities)
+    const checkActivityInputsErr = checkActivitiesInputs(activities);
     if (checkActivityInputsErr !== true) {
       return Promise.reject(new Error(checkActivityInputsErr));
     }
 
-    const hash = Encode(JSON.stringify(activities))
+    const hash = Encode(JSON.stringify(activities));
     if (hash === Cookies.getCookie('eb-lastactivity-hash')) {
       return Promise.resolve();
     }
 
     const url = `${Config.HTTP_PROTOCOL}${Config.API_URL}/tracker/${this.trackerKey}/activity`;
 
+    const segments = Eb.getNavigatorSegments();
+
     return fetch(url, {
       method: 'post',
       body: JSON.stringify({
-        activity: activities.map(x => ({ ...x, profile: this.profile.id }))
+        activity: activities.map(x => ({
+          ...x,
+          profile: this.profile.id,
+          segments,
+        })),
       }),
     })
       .then(x => x.json())
@@ -166,16 +204,23 @@ ${Config.API_URL}\
         return response;
       })
       .catch((err) => {
-        console.warn('Earlybirds error : trackActivity', err)
+        console.warn('Earlybirds error : trackActivity', err);
         throw err;
       });
   }
 
-  trackClickOnReco({ _id: { original_id }, id, url }) {
+  trackClickOnReco({
+    _id: {
+      original_id,
+    },
+    id,
+    url,
+  }) {
     this.trackActivity({
       verb: 'click-on-reco',
       originalId: id || original_id,
-    }).then(() => document.location.href = url)
+    })
+      .then(() => document.location.href = url)
       .catch((err) => {
         document.location.href = url;
       });
@@ -186,38 +231,34 @@ ${Config.API_URL}\
       return new Promise((resolve, reject) => {
         reject({
           message: 'Earlybirds error: Not identified'
-        })
-      })
+        });
+      });
     }
     if (!widgetId) {
       return new Promise((resolve, reject) => {
         reject({
           message: 'WidgetId is mandatory'
-        })
-      })
+        });
+      });
     }
     if (!verb) {
       return new Promise((resolve, reject) => {
         reject({
           message: 'Earlybirds error: Verb not provided'
-        })
-      })
+        });
+      });
     }
-    const url =
-      `${Config.HTTP_PROTOCOL}\
-${Config.API_URL}/widget/\
-${widgetId}/products-by-verb/\
-${this.profile.id}/${verb}`;
+    const url = `${Config.HTTP_PROTOCOL}${Config.API_URL}/widget/${widgetId}/products-by-verb/${this.profile.id}/${verb}`;
 
     return fetch(url, {
-      method: 'get'
-    })
-      .then(x => x.json())
-      .catch(err => {
-        console.warn('Earlybirds js : ', err.message)
-        throw err
+        method: 'get',
       })
+      .then(x => x.json())
+      .catch((err) => {
+        console.warn('Earlybirds js : ', err.message);
+        throw err;
+      });
   }
 }
 
-export default makeSingleton(Eb)
+export default makeSingleton(Eb);
